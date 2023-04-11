bf7ef7a9beb578a26fbf26505ab7e756a7c04e88
Caique Ferian <caiquegsf@gmail.com>
2023-04-11 18:12:50 -0300

    Adiciona exercicio 3 do desafio da Lexart Labs

---
+++ exercise_3.js
@@ -0,0 +1,11 @@
+const generateAlphanumericId = () => {
+  let id = "";
+  [1,2,3,4].forEach((_e,index) => {
+    if(index < 3) id += Math.random().toString(36).slice(2,6) + "-";
+    else id += Math.random().toString(36).slice(2,6);
+  });
+  return id;
+}
+
+const randomId = generateAlphanumericId();
+console.log(randomId);
